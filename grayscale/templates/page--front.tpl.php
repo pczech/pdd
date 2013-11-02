ddb2c7c81cdc9d9afb497ee630013a64d825dfef
pczech <pczech@wp.pl>
2013-11-02 23:54:44 +0100

    grayscale: added page--front.tpl.php

--- grayscale/templates/page--front.tpl.php
+++ grayscale/templates/page--front.tpl.php
@@ -123,6 +123,7 @@
     <div id="main-wrapper"><div id="main" class="clearfix">
 
       <div id="content" class="column"><div class="section">
+        <?php if ($is_front): ?><div id="welcome_message"><?php print "Welcome to My Site!"; ?></div><?php endif; ?>
         <?php if (!empty($page['highlighted'])): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
         <a id="main-content"></a>
         <?php print render($title_prefix); ?>
